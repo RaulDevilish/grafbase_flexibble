c752ff20e261437d1b5e76ec3a69da732be0cfa5
RaulDevilish <82582091+RaulDevilish@users.noreply.github.com>
2023-07-23 15:57:50 +0500

    Update grafbase.config.ts

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,41 +1,50 @@
-import { g, config, auth } from '@grafbase/sdk';
+import { g, auth, config } from '@grafbase/sdk'
 
-// @ts-ignore
+const post = g.model('Post', {
+  title: g.string(),
+  slug: g.string().unique(),
+  content: g.string().optional(),
+  publishedAt: g.datetime().optional(),
+  comments: g.relation(() => comment).optional().list().optional(),
+  likes: g.int().default(0),
+  tags: g.string().optional().list().length({ max: 5 }),
+  author: g.relation(() => user).optional()
+}).search()
+
+const comment = g.model('Comment', {
+  post: g.relation(post),
+  body: g.string(),
+  likes: g.int().default(0),
+  author: g.relation(() => user).optional()
+})
+
+const user = g.model('User', {
+  name: g.string(),
+  email: g.email().optional(),
+  posts: g.relation(post).optional().list(),
+  comments: g.relation(comment).optional().list()
+})
+
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+  name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
+  linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
-}).auth((rules) => {
-  rules.public().read()
 })
 
-// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
-  description: g.string(), 
+  description: g.string(),
   image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User),
-}).auth((rules) => {
-  rules.public().read()
-  rules.private().create().delete().update()
-})
-
-const jwt = auth.JWT({
-  issuer: 'grafbase',
-  secret:  g.env('NEXTAUTH_SECRET')
-})
+})  
 
 export default config({
-  schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private()
-  },
+  schema: g
 })
